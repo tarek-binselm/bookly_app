22fb2394ac80beff932299695b47d1b1b5ab1698
tarek-binselm <binselmt@gmail.com>
2023-11-05 03:54:30 +0200

    create splash view body

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:bookly_app/features/presentation/views/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      home: SplashView(),
+    );
+  }
+}
