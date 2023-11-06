8b1717e69fccebbec97bd7b7e1b328fcac10edc4
tarek-binselm <binselmt@gmail.com>
2023-11-07 01:33:11 +0200

    create app styles

--- lib/features/Home/presentation/views/widgets/home_view_body.dart
+++ lib/features/Home/presentation/views/widgets/home_view_body.dart
@@ -21,7 +21,7 @@ class HomeViewBody extends StatelessWidget {
           ),
           Text(
             'Best Seller',
-            style: Styles.titleMedium,
+            style: Styles.textStyle18,
           ),
           BestSellerListViewItem(),
         ],
