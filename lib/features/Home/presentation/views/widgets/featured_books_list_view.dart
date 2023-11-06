8e739df4ce8c6555272cd77d325aee27af5eea0c
tarek-binselm <binselmt@gmail.com>
2023-11-06 03:34:10 +0200

    add featured books list view to home

---
+++ lib/features/Home/presentation/views/widgets/featured_books_list_view.dart
@@ -0,0 +1,25 @@
+import 'package:bookly_app/features/Home/presentation/views/widgets/featured_books_list_view_item.dart';
+import 'package:flutter/material.dart';
+
+class FeaturedBooksListView extends StatelessWidget {
+  const FeaturedBooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .3,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 16),
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8),
+              child: FeaturedBooksListViewItem(),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/features/Home/presentation/views/widgets/featured_books_list_view_item.dart
@@ -0,0 +1,23 @@
+import 'package:bookly_app/core/utils/assests.dart';
+import 'package:flutter/material.dart';
+
+class FeaturedBooksListViewItem extends StatelessWidget {
+  const FeaturedBooksListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 2.7 / 4,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.red,
+          borderRadius: BorderRadius.circular(16),
+          image: const DecorationImage(
+            fit: BoxFit.fill,
+            image: AssetImage(AssetsData.testImage),
+          ),
+        ),
+      ),
+    );
+  }
+}
