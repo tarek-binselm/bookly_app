9dee8d124872ac7b89e69663c917693217fc96f5
tarek-binselm <binselmt@gmail.com>
2023-11-08 23:43:36 +0200

    create search view

---
+++ lib/features/Search/presentation/views/widgets/search_view_body.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SearchViewBody extends StatelessWidget {
+  const SearchViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const TextField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+      ),
+    );
+  }
+}
