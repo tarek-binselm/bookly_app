4b7c052e2036907358df9de0afac748f2206a630
tarek-binselm <binselmt@gmail.com>
2023-11-10 19:56:24 +0200

    create api service

---
+++ lib/features/Home/data/repos/home_repo_impl.dart
@@ -0,0 +1,18 @@
+import 'package:bookly_app/core/errors/failures.dart';
+import 'package:bookly_app/features/Home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/Home/data/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImpl implements HomeRepo {
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
+    // TODO: implement fetchBestSellerBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
+    // TODO: implement fetchFeaturedBooks
+    throw UnimplementedError();
+  }
+}
