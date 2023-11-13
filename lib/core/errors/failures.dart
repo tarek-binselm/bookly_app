526eb549fb13d6b98c316ec39b5eca5ad4b6f8c1
tarek-binselm <binselmt@gmail.com>
2023-11-13 20:56:01 +0200

    create server failure from dio exception

--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -1,8 +1,50 @@
-abstract class Failure {
+import 'package:dio/dio.dart';
 
+abstract class Failure {
+  final String errMessage;
+
+  const Failure(this.errMessage);
 }
 
 class ServerFailure extends Failure {
-  
-}
+  ServerFailure(super.errMessage);
 
+  factory ServerFailure.fromDioException(DioException dioException) {
+    switch (dioException.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure('connection timeout');
+      case DioExceptionType.sendTimeout:
+        return ServerFailure('send timeout');
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure('receive timeout');
+      case DioExceptionType.badCertificate:
+        return ServerFailure('bad certificate');
+      case DioExceptionType.badResponse:
+        return ServerFailure.fromResponse(
+            dioException.response!.statusCode!, dioException.response!.data);
+      case DioExceptionType.cancel:
+        return ServerFailure('request cancelled');
+      case DioExceptionType.connectionError:
+        return ServerFailure('connection error');
+      case DioExceptionType.unknown:
+        if (dioException.message!.contains('SocketException')) {
+          return ServerFailure('No Internet Connection');
+        }
+        return ServerFailure('Unexpected Error, Please try again');
+      default:
+        return ServerFailure('Oops There Was An Error, Please Try Again');
+    }
+  }
+
+  factory ServerFailure.fromResponse(int statusCode, dynamic response) {
+    if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+      return ServerFailure(response['error']['message']);
+    } else if (statusCode == 404) {
+      return ServerFailure('Your Request Not Found, Please Try Later!');
+    } else if (statusCode == 500) {
+      return ServerFailure('Internal Server Error, Please Try Later');
+    } else {
+      return ServerFailure('Oops There Was An Error, Please Try Again');
+    }
+  }
+}
